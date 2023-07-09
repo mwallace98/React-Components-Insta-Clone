556d6a6ed1f7d38aed93de6bfc2ad395f62f9ccf
Matt Wallace <mattwallace140@gmail.comgit config --global user.email mattwallace140@gmail.com>
2023-07-09 19:43:56 -0400

    Comment Section Displaying

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -9,11 +9,12 @@ const Comments = props => {
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */
-      comments.map((comment) => {
+      comments.map((comment) => (
         <Comment key={comment.id} comment= {comment}/>
-      })}
+      ))}
     </div>
   );
 };
 
 export default Comments;
+
